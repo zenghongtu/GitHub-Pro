ebfd374879df6681d873504d90118f26522492ad
zenghongtu <zenghongtu@gmail.com>
2019-08-26 21:49:38 +0800

    Feat: update navbar style

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,29 +1,55 @@
-import Taro, { Component, useState } from "@tarojs/taro"
-import { View, Text, Button } from "@tarojs/components"
+import Taro, {
+  Component,
+  useState,
+  navigateTo,
+  useDidShow,
+  useEffect
+} from "@tarojs/taro"
+import { View, Text, Button, Image, Block } from "@tarojs/components"
 import { AtNavBar, AtDrawer } from "taro-ui"
+import logo from "../../assets/logo.png"
 import "./index.scss"
-
-const marginTop = Taro.getSystemInfoSync().statusBarHeight
-
-const styleProps = { marginTop: `${marginTop}px` }
+import { getGlobalData } from "../../utils/global_data"
+import { IUserInfo } from "../../services/user"
 
 export interface NavBarProps {
   title?: string
+  path?: string
   isGoBackBtn?: boolean
   leftIconType?: string
   onClickLeftIcon?: () => {}
 }
 
+const marginTop = Taro.getSystemInfoSync().statusBarHeight
+
+const styleProps = { marginTop: `${marginTop}px` }
+
+const navList = [
+  { name: "profile", path: "profile" },
+  { name: "trending", path: "trending" },
+  { name: "starred", path: "starred" }
+]
+
 const NavBar = ({
   title = "",
+  path = "",
   isGoBackBtn = false,
   leftIconType = "menu",
   onClickLeftIcon
 }: NavBarProps) => {
+  const curUserInfo = getGlobalData("userInfo") as IUserInfo
+
+  const [userInfo, setUserInfo] = useState<IUserInfo>(curUserInfo)
   const [isShowDrawer, setShowDrawer] = useState(false)
 
-  const handleToggleDrawer = () => {
-    setShowDrawer(!isShowDrawer)
+  useDidShow(() => {
+    if (!userInfo) {
+      setUserInfo(getGlobalData("userInfo") as IUserInfo)
+    }
+  })
+
+  const handleToggleDrawer = (isShow = true) => {
+    setShowDrawer(isShow)
   }
 
   const handleClickLeftIcon = () => {
@@ -32,6 +58,13 @@ const NavBar = ({
     } else {
       ;(onClickLeftIcon || handleToggleDrawer)()
     }
+  }
+  const handleNavItemClick = path => _ => {
+    Taro.navigateTo({ url: `/pages/${path}/index` })
+  }
+
+  const handleClickLogo = () => {
+    Taro.navigateTo({ url: "/pages/login/index" })
   }
 
   const curLeftIconType = isGoBackBtn ? "chevron-left" : leftIconType
@@ -43,9 +76,46 @@ const NavBar = ({
         title={title}
         leftIconType={curLeftIconType}
       />
-      <AtDrawer show={isShowDrawer} onClose={handleToggleDrawer}>
-        <View style={styleProps}>
-          <View className="drawer-item">Drawer</View>
+      <AtDrawer
+        show={isShowDrawer}
+        onClose={handleToggleDrawer.bind(this, false)}
+      >
+        <View className="drawer" style={styleProps}>
+          <View className="drawer-header">
+            {userInfo ? (
+              <Block>
+                <Image
+                  className="avatar"
+                  mode="widthFix"
+                  src={userInfo.avatar_url}
+                ></Image>
+                <View className="name">{userInfo.name}</View>
+                <View className="bio">{userInfo.bio}</View>
+              </Block>
+            ) : (
+              <Block>
+                <Image
+                  className="avatar"
+                  mode="widthFix"
+                  src={logo}
+                  onClick={handleClickLogo}
+                ></Image>
+                <View>点击登录</View>
+              </Block>
+            )}
+          </View>
+          {navList.map(item => {
+            const { name, path } = item
+            return (
+              <View
+                className="nav-item"
+                key={path}
+                onClick={handleNavItemClick(path)}
+              >
+                {name}
+              </View>
+            )
+          })}
         </View>
       </AtDrawer>
     </View>
