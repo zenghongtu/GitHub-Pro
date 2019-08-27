0a38819068f6b1be6cd0f07a5ff598c838ac8c13
zenghongtu <zenghongtu@gmail.com>
2019-08-27 23:40:01 +0800

    Chore: update config

--- client/config/dev.js
+++ client/config/dev.js
@@ -1,9 +1,33 @@
+const path = require("path")
+
 module.exports = {
   env: {
     NODE_ENV: '"development"',
     ClOUD_ENV: '"dev-002r4"'
   },
   defineConstants: {},
-  weapp: {},
+  alias: {
+    "@/components": path.resolve(__dirname, "..", "src/components"),
+    "@/utils": path.resolve(__dirname, "..", "src/utils"),
+    "@/hooks": path.resolve(__dirname, "..", "src/hooks"),
+    "@/assets": path.resolve(__dirname, "..", "src/assets"),
+    "@/services": path.resolve(__dirname, "..", "src/services"),
+    "@/constants": path.resolve(__dirname, "..", "constants.js"),
+    "@/package": path.resolve(__dirname, "..", "package.json")
+  },
+  copy: {
+    patterns: [
+      {
+        from: "src/wemark",
+        to: "dist/wemark"
+      }
+    ],
+    options: {}
+  },
+  weapp: {
+    compile: {
+      exclude: ["src/wemark/remarkable.js"]
+    }
+  },
   h5: {}
 }
