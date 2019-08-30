b4e3f7fbed9d797d26fdd73be74b31fb17dcfbe2
zenghongtu <zenghongtu@gmail.com>
2019-08-30 22:29:04 +0800

    Feat: update app bg

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,8 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import Index from './pages/index'
+import "@tarojs/async-await"
+import Taro, { Component, Config } from "@tarojs/taro"
+import Index from "./pages/trending/index"
 
-import './app.scss'
+import "./app.scss"
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
@@ -10,7 +11,6 @@ import './app.scss'
 // }
 
 class App extends Component {
-
   /**
    * 指定config的类型声明为: Taro.Config
    *
@@ -20,36 +20,46 @@ class App extends Component {
    */
   config: Config = {
     pages: [
-      'pages/index/index'
+      "pages/trending/index",
+      "pages/activity/index",
+      "pages/repos/content/index",
+      "pages/repos/index",
+      "pages/repos/files/index",
+      "pages/profile/index",
+      "pages/starred/index",
+      "pages/login/index",
+      "pages/my-languages/index"
     ],
     window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
+      navigationStyle: "custom",
+      backgroundTextStyle: "light",
+      navigationBarBackgroundColor: "#fff",
+      navigationBarTitleText: "WeChat",
+      navigationBarTextStyle: "black"
+      // TODO use scroll-view
+      // enablePullDownRefresh: true
     },
     cloud: true
   }
 
-  componentDidMount () {
-    if (process.env.TARO_ENV === 'weapp') {
-      Taro.cloud.init()
+  componentDidMount() {
+    if (process.env.TARO_ENV === "weapp") {
+      const env = process.env.ClOUD_ENV
+      Taro.cloud.init({ env, traceUser: true })
     }
   }
 
-  componentDidShow () {}
+  componentDidShow() {}
 
-  componentDidHide () {}
+  componentDidHide() {}
 
-  componentDidCatchError () {}
+  componentDidCatchError() {}
 
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
-  render () {
-    return (
-      <Index />
-    )
+  render() {
+    return <Index />
   }
 }
 
-Taro.render(<App />, document.getElementById('app'))
+Taro.render(<App />, document.getElementById("app"))
