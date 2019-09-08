701d1bf0d5dd37f6203bad263d74f4b3e8917dc1
zenghongtu <zenghongtu@gmail.com>
2019-09-08 21:21:14 +0800

    feat: add create comment

---
+++ client/src/pages/issues/create-comment/index.tsx
@@ -0,0 +1,51 @@
+import Taro, { Component, Config, useState, useRouter } from "@tarojs/taro"
+import { View, Text, Button } from "@tarojs/components"
+import "./index.scss"
+import { AtInput, AtTextarea, AtButton } from "taro-ui"
+import { createIssueComment } from "../../../services/issues"
+
+const CreateComment = () => {
+  const {
+    params: { full_name, number }
+  } = useRouter()
+
+  const [content, setContent] = useState("")
+
+  const handleTextareaChange = ev => {
+    setContent(ev.target.value)
+  }
+  const handleConfirm = () => {
+    const data = {
+      body: content
+    }
+
+    createIssueComment({ full_name, number }, data).then(res => {
+      if (res && res.body === content) {
+        Taro.showToast({ title: "Success", icon: "success" })
+        setTimeout(() => {
+          Taro.navigateBack()
+        }, 1000)
+      }
+    })
+  }
+
+  return (
+    <View className="wrap">
+      <View className="content">
+        <AtTextarea
+          height={400}
+          count={false}
+          maxLength={10000}
+          value={content}
+          onChange={handleTextareaChange}
+          placeholder="Leave a comment"
+        />
+      </View>
+      <AtButton className="confirm" type="primary" onClick={handleConfirm}>
+        Comment
+      </AtButton>
+    </View>
+  )
+}
+
+export default CreateComment
