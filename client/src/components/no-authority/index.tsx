79f5e4a5b5b88813a092cd11ea55742bb0d5b324
zenghongtu <zenghongtu@gmail.com>
2019-09-15 00:56:24 +0800

    feat: add no authority page

---
+++ client/src/components/no-authority/index.tsx
@@ -0,0 +1,29 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Image, Button } from '@tarojs/components'
+import './index.scss'
+import img from '@/assets/ironcat.jpg'
+import { AtButton } from 'taro-ui'
+
+const NoAuthority = () => {
+  return (
+    <View className="wrap">
+      <View className="inner">
+        <Image src={img} className="img"></Image>
+        <View className="desc">This page require login!</View>
+        <View className="login">
+          <AtButton
+            size="small"
+            type="primary"
+            onClick={() => {
+              Taro.navigateTo({ url: `/pages/login/index` })
+            }}
+          >
+            Go to Login
+          </AtButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default NoAuthority
