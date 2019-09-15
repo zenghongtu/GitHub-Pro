8acde84f801560097ad6d1aae9be78d9f1aab436
zenghongtu <zenghongtu@gmail.com>
2019-09-15 19:06:20 +0800

    chore: update prod config

--- client/config/prod.js
+++ client/config/prod.js
@@ -1,18 +1,47 @@
+const path = require('path')
+
 module.exports = {
   env: {
     NODE_ENV: '"production"',
     ClOUD_ENV: '"prod-gxbyo"'
   },
   defineConstants: {},
-  weapp: {},
-  h5: {
-    /**
-     * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
-     * 参考代码如下：
-     * webpackChain (chain) {
-     *   chain.plugin('analyzer')
-     *     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
-     * }
-     */
-  }
+  alias: {
+    '@/components': path.resolve(__dirname, '..', 'src/components'),
+    '@/utils': path.resolve(__dirname, '..', 'src/utils'),
+    '@/hooks': path.resolve(__dirname, '..', 'src/hooks'),
+    '@/assets': path.resolve(__dirname, '..', 'src/assets'),
+    '@/services': path.resolve(__dirname, '..', 'src/services'),
+    '@/constants': path.resolve(__dirname, '..', 'src/constants.ts'),
+    '@/package': path.resolve(__dirname, '..', 'package.json')
+  },
+  copy: {
+    patterns: [
+      {
+        from: 'src/wemark',
+        to: 'dist/wemark'
+      }
+    ],
+    options: {}
+  },
+  weapp: {
+    module: {
+      postcss: {
+        autoprefixer: {
+          enable: true
+        },
+        // 小程序端样式引用本地资源内联配置
+        url: {
+          enable: true,
+          config: {
+            limit: 10240 // 文件大小限制
+          }
+        }
+      }
+    },
+    compile: {
+      exclude: ['src/wemark/remarkable.js']
+    }
+  },
+  h5: {}
 }
