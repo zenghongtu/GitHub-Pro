f4784afe9d4ade9f9ffc9b93b381255525ad42e5
zenghongtu <zenghongtu@gmail.com>
2019-09-17 08:29:51 +0800

    feat: add update modal

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -141,6 +141,36 @@ class App extends Component {
 
   componentDidCatchError() {}
 
+  updateApp() {
+    if (Taro.canIUse('getUpdateManager')) {
+      const updateManager = Taro.getUpdateManager()
+      updateManager.onCheckForUpdate(res => {
+        // 请求完新版本信息的回调
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(() => {
+            Taro.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              success: res => {
+                if (res.confirm) {
+                  // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                  updateManager.applyUpdate()
+                }
+              }
+            })
+          })
+          updateManager.onUpdateFailed(() => {
+            // 新的版本下载失败
+            Taro.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+            })
+          })
+        }
+      })
+    }
+  }
+
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
   render() {
